fd3de6c2e977f85074f0d233291e8ce96913984b
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-21 15:59:41 +0800

    之后思考

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import tulind from 'tulind';
 
+console.log(tulind.indicators.sma);
+
 tulind.indicators.sma.indicator([[1, 2, 3]], [2], (error, data) => {
   if (error) throw error;
   console.log(data);
