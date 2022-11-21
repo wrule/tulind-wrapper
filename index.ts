45e14a6309a50d0f4d45771cd87245f8a676eda4
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-21 15:34:27 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
+import tulind from 'tulind';
 
+console.log(tulind.indicators.abs.indicator([], [], (e, data) => {
+
+}));
+console.log(Object.keys(tulind.indicators.abs));
---
+++ tulind.d.ts
@@ -0,0 +1,21 @@
+
+interface indicator {
+  name: string;
+  full_name: string;
+  type: string;
+  inputs: number;
+  options: number;
+  outputs: number;
+  input_names: string[];
+  option_names: string[];
+  output_names: string[];
+  indicator: (inputs: number[][], options: number[], callback: (error: Error, data: number[][]) => void) => void;
+  start: (options: number[]) => number;
+}
+
+declare module 'tulind' {
+  export const version: string;
+  export const indicators: {
+    [key: string]: indicator;
+  };
+};
