644b22f51aa6ebf15f4a461505459189812852cc
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-22 16:44:19 +0800

    对齐功能基本开发完成

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 import { macd, sma } from '.';
 
 async function main() {
-  const result = macd([1, 2, 3, 3, 2, 3, 4, 8, 7], { short_period: 2, long_period: 3, signal_period: 4 });
+  const result = macd([1, 2, 3, 3, 2, 3, 4, 8, 7], { short_period: 2, long_period: 3, signal_period: 4 }, true);
   console.log(result);
 }
 
