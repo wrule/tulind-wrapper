9c0aa6e16e39823c7e6abf0fe6185656c67e33a8
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-22 12:28:43 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -21,21 +21,23 @@ export
 function typescript_code(indic: indicator) {
   return `
 export
-function ${
-  indic.name
-}(${
-  indic.input_names.map((name) => `${name}: number[]`).join(', ')
+function ${indic.name}(${indic.input_names.map((name) => `${name}: number[]`).join(', ')}${
+  indic.option_names.length < 1 ?
+    '' :
+    `, options: { ${indic.option_names.map((name) => `${name}: number`).join('; ')} }`
 }) {
-  const result = converter('${indic.name}', [${indic.input_names.join(', ')}], []);
+  const result = converter('${indic.name}', [${indic.input_names.join(', ')}], [${
+    indic.option_names.length < 1 ? '' : indic.option_names.map((name) => `options.${name}`).join(', ')
+  }]);
   return { ${indic.output_names.map((name, index) => `${name}: result[${index}]`).join(', ')} };
 }
   `.trim();
 }
 
 async function main() {
-  console.log(typescript_code(tulind.indicators.macd));
-  console.log(sma([2, 3, 1, 3, 4], 2));
-  console.log(tulind.indicators.macd);
+  console.log(typescript_code(tulind.indicators.stoch));
+  // console.log(sma([2, 3, 1, 3, 4], 2));
+  console.log(tulind.indicators.stoch);
 }
 
 main();
