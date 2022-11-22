5992214b6f131fa536c87e57f05ddaa3f3ed4c6e
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-22 17:03:02 +0800

    修改部分测试代码

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 import { macd, macd_start } from '.';
 
-async function main() {
+function main() {
   const result = macd([1, 2, 3, 3, 2, 3, 4, 8, 7], { short_period: 2, long_period: 3, signal_period: 4 }, true);
   console.log(result);
   const start = macd_start({ short_period: 2, long_period: 3, signal_period: 4 });
