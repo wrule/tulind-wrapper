4039e9c2669bf4c70e72e3a5c92814a90df8a6e0
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-22 17:11:24 +0800

    d.ts

---
+++ generator.d.ts
@@ -0,0 +1,5 @@
+export declare function norm(text: string): string;
+export declare function typescript_code(indic: Indicator): string;
+export declare function _align(outputs: number[][], length: number): void;
+export declare function _converter(name: string, inputs: number[][], options: number[], align?: boolean): number[][];
+//# sourceMappingURL=generator.d.ts.map
