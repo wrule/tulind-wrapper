7696e614ff7a0e8c8f87cb56b6f5d696448722cb
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-22 10:57:07 +0800

    验证无误

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 import tulind from 'tulind';
 
-console.log(tulind.indicators.sma);
 
-tulind.indicators.sma.indicator([[1, 2, 3]], [2], (error, data) => {
-  if (error) throw error;
-  console.log(data);
-});
+async function main() {
+  let result: string[] = [];
+  Object
+    .values(tulind.indicators)
+    .map((value) => Object.keys(value))
+    .forEach((item) => {
+      result.push(...item);
+    });
+  result = Array.from(new Set(result));
+  console.log(result);
+}
+// console.log(tulind.indicators.sma);
+main();
