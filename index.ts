8d4eb185470ba9a34429a1d3d1e69e5bba3200b0
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-22 11:26:12 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -1,7 +1,45 @@
 import tulind from 'tulind';
 
-async function main() {
+export
+function sma(real: number[], period: number) {
+  let sma: number[] = [];
+  tulind.indicators.sma.indicator([], [], (error, data) => {
+    if (error) throw error;
+    sma = data[0];
+  });
+  return sma;
+}
 
+export
+function typescript_code(indic: indicator) {
+  return `
+export
+function ${
+  indic.name
+}(${
+  indic.input_names.map((name) => `${name}: number[]`).join(', ')
+}, ${
+  (() => {
+    if (indic.option_names.length < 1) return '';
+    if (indic.option_names.length < 2) return `${indic.option_names[0]}: number`;
+    return `options: { ${indic.option_names.map((name) => `${name}: number[];`).join(' ')} }`;
+  })()
+}) {
+  let sma: number[] = [];
+  tulind.indicators.${
+  indic.name
+}.indicator([], [], (error, data) => {
+    if (error) throw error;
+    sma = data[0];
+  });
+  return sma;
+}
+`.trim();
+}
+
+async function main() {
+  console.log(tulind.indicators.macd);
+  console.log(typescript_code(tulind.indicators.macd));
 }
 
 main();
