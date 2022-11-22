e5bf930d26e7e3fd91db54f1fe2500f1a9908911
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-22 16:25:34 +0800

    提交代码

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
-console.log('你好，世界');
+import { macd, sma } from '.';
+
+async function main() {
+  const result = macd([1, 2, 3, 3, 2, 3, 4, 8, 7], { short_period: 2, long_period: 3, signal_period: 4 });
+  console.log(result);
+}
+
+main();
