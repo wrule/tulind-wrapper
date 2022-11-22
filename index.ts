d7ee9057ece9a919d4510c76ef7354695667baa6
jimao <jimao@jimaodeMacBook-Pro.local>
2022-11-22 10:57:54 +0800

    提交代码

--- index.ts
+++ index.ts
@@ -1,16 +1,7 @@
 import tulind from 'tulind';
 
-
 async function main() {
-  let result: string[] = [];
-  Object
-    .values(tulind.indicators)
-    .map((value) => Object.keys(value))
-    .forEach((item) => {
-      result.push(...item);
-    });
-  result = Array.from(new Set(result));
-  console.log(result);
+
 }
-// console.log(tulind.indicators.sma);
+
 main();
